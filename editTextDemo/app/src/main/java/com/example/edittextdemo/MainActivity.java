ae55d92aa63de118f8b8b990fed99dbfee5d949e
A111223006 <A111223006@mail.shu.edu.tw>
2024-03-15 14:25:20 +0800

    2024-03-15

---
+++ editTextDemo/app/src/main/java/com/example/edittextdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.edittextdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public  void botten_Click(View view){
+        EditText editName = (EditText) findViewById(R.id.editName);
+        TextView txvShow = (TextView)  findViewById(R.id.txvShow);
+        String name = editName.getText().toString();
+        txvShow.setText("你的名子是:" + name);
+    }
+}
